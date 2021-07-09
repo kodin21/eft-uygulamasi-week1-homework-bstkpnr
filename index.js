5b55f27102dad53ce25bf74b084bd6c04f64d90c
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2021-07-10 01:54:46 +0300

    update fixed

--- index.js
+++ index.js
@@ -1,78 +1,95 @@
 // import { name } from './example-other-file';
 
+
 // console.log('Javascriptinizi buraya yazabilirsiniz')
 
 // // Baska dosyadan calistirmak mumkun, dosyalari parcalamaktan korkmayin!
 // // name
 const accounts = [
-    {
-      iban: "trxx yyyy xxxx yyyy",
-      balance: 100,
-    },
-    {
-      iban: "trxx yyyy xxxx yyyx",
-      balance: 5040,
-    },
-    {
-      iban: "trxx yyyy xxxx xyyx",
-      balance: 10594,
-    },
-  ];
-  var dropdown = document.getElementById("bankaHesabi");
-  var accounting = document.createElement("option");
-  accounting.innerHTML = accounts.map(function (e) {
-    var account_number = `Hesap No: ${e.iban} - Bakiye: ${e.balance} £`;
-    console.log(account_number);
-    var hesaplar = document.createElement("option");
-    hesaplar.innerText = account_number;
-    dropdown.appendChild(hesaplar);
+  {
+    iban: "trxx yyyy xxxx yyyy",
+    balance: 100,
+  },
+  {
+    iban: "trxx yyyy xxxx yyyx",
+    balance: 5040,
+  },
+  {
+    iban: "trxx yyyy xxxx xyyx",
+    balance: 10594,
+  },
+];
+let sendMoney = 0;
+let balance = 0;
+var dropdown = document.getElementById("bankAccount");
+ dropdown && accounts.forEach(function (e) {
+  var account_number = `Hesap No: ${e.iban} - Bakiye: ${e.balance} £`;
+     var accounts = document.createElement("option");
+     accounts.innerText = account_number;
+     dropdown.appendChild(accounts);
+     activeButton();
+});
+
+function activeButton() {
+  if (sendMoney && balance) {
+    document.getElementById("sendAccount").disabled = false;
+  } else {
+    document.getElementById("sendAccount").diabled = true;
+  }
+}
+let interval;
+function startTimer(duration, display) {
+  let start = Date.now(),
+    diff =null,
+    minutes=null,
+    seconds=null;
+
+  function timer() {
+    diff = duration - (((Date.now() - start) / 1000) | 0);
+    minutes = (diff / 60) | 0;
+    seconds = diff % 60 | 0;
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    display &&  (display.textContent = minutes + ":" + seconds);
+
+
+    if (minutes == 0 && seconds == 0) {
+      clearInterval(interval);
+      alert("SÜRE BİTTİ ");
+      setTimeout(function(){
+        location.href='index.html'
+      },1000);
+    } 
+  }
+  interval=setInterval(timer, 120);
+}
+window.onload = function () {
+  var twoMinutes = 60 * 2;
+  display = document.querySelector("#time");
+  startTimer(twoMinutes, display);
+};
+let sendAccount=document.getElementById('sendAccount');
+sendAccount && sendAccount.addEventListener('click',function () {
+  document.querySelector('#aliciIban').value="";
+  setTimeout(function () {
+    location.href = "index2.html";
+    console.log("Basıldı", sendAccount);
   });
-  function startTimer(duration, display) {
-    var start = Date.now(),
-      diff,
-      minutes,
-      seconds;
+})
   
-    function timer() {
-      diff = duration - (((Date.now() - start) / 1000) | 0);
-      minutes = (diff / 60) | 0;
-      seconds = diff % 60 | 0;
-  
-      minutes = minutes < 10 ? "0" + minutes : minutes;
-      seconds = seconds < 10 ? "0" + seconds : seconds;
-  
-      display.textContent = minutes + ":" + seconds;
-  
-      if (display <= 0) {
-        start = Date.now() + 1000;
-      }
-    }
-    timer();
-    setInterval(timer, 1000);
-  }
-  window.onload = function () {
-    var twoMinutes = 60 * 2;
-    display = document.querySelector("#time");
-    startTimer(twoMinutes, display);
-  };
-  
-  document.getElementById("yonlendir").onclick = function () {
-    window.setTimeout(function () {
-      location.href = "index2.html";
-    });
-  };
-  
-  function yolla() {
-    var pasword = document.getElementById("sifre").value;
-  
-    if (pasword.length == 4) {
-      if (pasword === "1234") {
-        alert("Başarılı");
-      } else {
+
+var counter = 0;
+function enter() {
+  var pasword = document.getElementById("sifre").value;
+
+  if (pasword.length == 4) {
+    if (pasword === "1234") {
+      alert("Başarılı");
+    }else
         alert("ŞİFRE HATALI !!!");
-      }
-    } else {
-      alert("KARAKTER UZUNLUĞU 4 OLMALI");
+        return;
+      
     }
   }
-  
+
